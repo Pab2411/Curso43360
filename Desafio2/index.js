6e115c2e6cf3a1641333d4baf98dc8fa69b9eb5b
pab2411 <pablosivina@gmail.com>
2023-05-27 20:20:01 -0300

    desafio 2 completo

--- Desafio2/index.js
+++ Desafio2/index.js
@@ -43,41 +43,150 @@ class ProductManager {
             console.log('Error al agregar el producto:', error);
         }
     }
-    getProducts() {
-        return this.products;
-    }
+    async getProducts() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, "utf-8");
+                if (data) {
+                    this.products = JSON.parse(data);
+                    console.log(this.products)
 
-    getProductById(id) {
-        const product = this.products.find((p) => p.id === id);
-        if (!product) {
-            console.log('Error: Producto no encontrado');
-            return;
+                    if (!this.products || this.products.length === 0) {
+                        console.log('Error: No hay productos');
+                    }
+
+                } else {
+                    console.log('Error: No hay productos');
+                }
+            } else {
+                console.log('Error: El archivo no existe')
+            }
+        } catch (error) {
+            console.log("Error No hay productos :", error);
+
         }
-
-        return product;
     }
+    async getProductById(id) {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, "utf-8");
+                if (data) {
+                    this.products = JSON.parse(data);
+
+                    const product = await this.products.find((p) => p.id === id);
+                    if (product) {
+                        console.log(product)
+                        return product;
+                    } else {
+                        console.log(`Error: Producto no encontrado con el id ${id}`);
+                    }
+
+                }
+            } else {
+                console.log('Error: El archivo no existe')
+            }
+        } catch (error) {
+            console.log("Error el producto no se encontro :", error);
+        }
+    }
+
+    async updateProduct(id, updatedFields) {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, "utf-8");
+                if (data) {
+                    this.products = JSON.parse(data);
+
+                    const productIndex = this.products.findIndex((p) => p.id === id);
+                    if (productIndex === -1) {
+                        console.log(`Error: Producto no encontrado con el id ${id}`);
+                        return;
+                    }
+
+                    const updatedProduct = {
+                        ...this.products[productIndex],
+                        ...updatedFields,
+                    };
+
+                    this.products[productIndex] = updatedProduct;
+
+                    await fs.promises.writeFile(
+                        this.path,
+                        JSON.stringify(this.products, null, "\t")
+                    );
+
+                    console.log("Producto actualizado correctamente");
+                    console.log(updatedProduct)
+                } else {
+                    console.log("Error: No hay productos");
+                }
+            } else {
+                console.log("Error: El archivo no existe");
+            }
+        } catch (error) {
+            console.log("Error al actualizar el producto:", error);
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, 'utf-8');
+                if (data) {
+                    this.products = JSON.parse(data);
+
+                    const productIndex = this.products.findIndex((p) => p.id === id);
+
+                    if (productIndex !== -1) {
+                        this.products.splice(productIndex, 1);
+
+                        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
+
+                        console.log('Producto eliminado correctamente');
+                    } else {
+                        console.log(`Error: No se encontró el producto con el id ${id}`);
+                    }
+                }
+            } else {
+                console.log('Error: El archivo no existe');
+            }
+        } catch (error) {
+            console.log('Error al eliminar el producto:', error);
+        }
+    }
+
 
 }
 
 
 const productManager = new ProductManager(archivo);
 
-const products = productManager.getProducts();
-console.log(products);
+/*const products = async () => {
 
-productManager.addProduct({
-    title: "producto prueba 9",
+    let resultado = await productManager.getProducts();
+    console.log(resultado);
+}
+products();*/
+
+const product = productManager.getProducts();
+
+/*
+const cargoProducto = productManager.addProduct({
+    title: "producto prueba 11",
     description: "Este es un producto prueba 3",
-    price: 400,
+    price: 1000,
     thumbnail: "Sin imagen",
-    code: "abc5",
-    stock: 27
+    code: "abcd",
+    stock: 30
 })
 
 
 
-/*
 
-const product = productManager.getProductById(1);
-console.log(product);
+
+const buscoProducConId = productManager.getProductById(4);
+/*
+const modificoProdconId = productManager.updateProduct(4, { title: "se va ", price: 800 })
+
+const eliminoConId = productManager.deleteProduct(4)
 */
