4a387ee2a1bde513676c7b36a97a622c10ada02d
Pab2411 <pablosivina@gmail.com>
2023-07-13 23:22:51 -0300

    desafio 5

--- Desafio5/src/dao/dbManager/products.js
+++ Desafio5/src/dao/dbManager/products.js
@@ -39,23 +39,7 @@ export default class Products {
                 return { error: 'Todos los campos son obligatorios' };
             }
 
-
-            /*   const products = await productsModel.find().lean();
-   
-               const lastProduct = products[products.length - 1];
-               const productId = lastProduct ? lastProduct.id + 1 : 1;
-   
-
-            const products = await productsModel.find().lean();
-
-            const lastProduct = products[products.length - 1];
-            const lastProductId = lastProduct ? lastProduct.id : '0';
-
-            const productId = String(parseInt(lastProductId) + 1).toString();
-*/
             const newProduct = new productsModel({
-                //id: productId,
-
                 title,
                 description,
                 code,
@@ -106,31 +90,6 @@ export default class Products {
         }
     };
 
-
-    /*      updateProduct = async (productId, updatedData) => {
-              try {
-                  const { title, description, code, price, stock, category, thumbnails } = updatedData;
-                  if (!title && !description && !code && !price && !stock && !category) {
-                      return { error: 'Necesita ingresar al menos un campo para poder modificar' };
-                  }
-      
-                  const product = await productsModel.findOne({ id: productId });
-                  if (product) {
-                      const updatedProduct = {
-                          ...product.toObject(),
-                          ...updatedData
-                      };
-                      await productsModel.updateOne({ id: productId }, updatedProduct);
-                      return { status: "success", message: "Producto actualizado con éxito" };
-                  } else {
-                      return { error: 'Producto no encontrado' };
-                  }
-              } catch (error) {
-                  console.error(error);
-                  return { error: 'Error al actualizar el producto' };
-              }
-          }
-      */
     // Borro un producto por ID
 
     deleteProduct = async (_id) => {
@@ -149,23 +108,4 @@ export default class Products {
             return { error: 'Error al eliminar el producto' };
         }
     };
-
-    /*
-        deleteProduct = async (productId) => {
-            try {
-                const products = await productsModel.find().lean();
-                const productIndex = products.findIndex((p) => p.id === productId);
-                if (productIndex !== -1) {
-                    const deletedProduct = products.splice(productIndex, 1)[0];
-                    await productsModel.deleteOne({ id: productId });
-                    return { status: "success", message: "Producto borrado", detalle: deletedProduct };
-                } else {
-                    return { error: 'Producto no encontrado' };
-                }
-            } catch (error) {
-                console.error(error);
-                return { error: 'Error al eliminar el producto' };
-            }
-        }
-       */
 }
